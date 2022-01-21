2672dea5ab37937458bc2cb7cbe5317ea6d653d1
khushali452 <khushalivasani.ict19@gmail.com>
2022-01-21 22:41:11 +0530

    updated

---
+++ simplify/src/components/AddContact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class AddContact extends Component {
+  render() {
+    return <div>
+        <div className='ui main'>
+            <h2>Add Contact</h2>
+            <form className='ui form'>
+                <div className='field'>
+                    <label>Name</label>
+                    <input type="text" name="name" placeholder="Name" />
+                </div>
+                <div className='field'>
+                    <label>Email</label>
+                    <input type= "email" name="email" placeholder="Email" />
+                </div>
+                <button className='ui button blue'>Add</button>
+            </form>
+        </div>
+    </div>;
+  }
+}
