64166f097ba48172fee084f8affcf6afb2412e19
khushali452 <khushalivasani.ict19@gmail.com>
2022-01-22 21:32:34 +0530

    .

--- simplify/src/components/ContactList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const ContactList = () => {
-  return <div className='ui celled list'>
-      Contact List
-  </div>;
-};
-export default ContactList;
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import './App.css';
 
-import Header from "../../../src/components/Header";
-import AddContact from "../../../src/components/AddContact";
-import ContactList from "../../../src/components/ContactList";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 
 function App() {
