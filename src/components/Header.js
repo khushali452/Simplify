7c9e2f00c992f90f94b794dd0ef747ddf84ad41d
khushali452 <khushalivasani.ict19@gmail.com>
2022-01-23 17:04:32 +0530

    Completed Basics

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-  return <div className="ui fixed menu">
-      <div className='ui container center'>
-          <h2>Simplify</h2>
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <h2>Contact Manager</h2>
       </div>
-
-  </div>;
+    </div>
+  );
 };
+
 export default Header;
